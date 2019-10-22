8ea0f235dd09b1326dec8888bd39a1aaff1f99a7
Reed Cozart <Reed.Cozart@ibm.com>
2019-10-22 14:43:39 -0500

    removing appmetrics from package.swift and package.resolved

--- Package.swift
+++ Package.swift
@@ -7,12 +7,11 @@ let package = Package(
       .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.8.1")),
       .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.9.0"),
       .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", from: "9.1.0"),
-      .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.6.4"),
       .package(url: "https://github.com/IBM-Swift/Health.git", from: "1.0.5"),
     ],
     targets: [
       .target(name: "swiftwebapp", dependencies: [ .target(name: "Application"), "Kitura" , "HeliumLogger"]),
-      .target(name: "Application", dependencies: [ "Kitura", "CloudEnvironment","SwiftMetrics", "Health", 
+      .target(name: "Application", dependencies: [ "Kitura", "CloudEnvironment","Health", 
 
       ]),
 
